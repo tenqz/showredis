0d9feb11c6e532f5bc1eca07ad82aa7e21e4a7d1
tenqz <smmartbiz@gmail.com>
2022-04-17 16:57:42 +0300

    Added Ant vue

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,9 @@ import {createApp} from "vue"
 import {createRouter, createWebHistory} from "vue-router"
 import routes from "./routes"
 import RedisClient from "./vue/RedisClient"
+import Antd from "ant-design-vue"
+import "ant-design-vue/dist/antd.css"
+
 
 const router = createRouter({
     history: createWebHistory(),
@@ -10,4 +13,5 @@ const router = createRouter({
 
 const app = createApp(RedisClient)
 app.use(router)
+app.use(Antd)
 app.mount("#show-redis")
--- assets/js/vue/RedisClient.vue
+++ assets/js/vue/RedisClient.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-   <p>ShowRedis</p>
-  </div>
+  <a-layout has-sider>
+    <a-layout-sider
+        :style="{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0, top: 0, bottom: 0 }"
+    >
+      <div class="logo">
+        <span>ShowRedis</span>
+      </div>
+    </a-layout-sider>
+    <a-layout :style="{ marginLeft: '200px' }">
+      <a-layout-content :style="{ margin: '24px 16px 0', overflow: 'initial' }">
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
 
 <script>
@@ -9,3 +19,14 @@ export default {
   name: "RedisClient",
 }
 </script>
+<style>
+.ant-layout-sider {
+  background-color: #282828;
+}
+.logo {
+  height: 32px;
+  color: #fff;
+  text-align: center;
+  font-size: 2em;
+}
+</style>
